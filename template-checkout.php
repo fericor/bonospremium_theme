85478932dba3a9623519b8e42b0eb62f0f1b5fef
Felix Cortez <fericor@hotmail.com>
2026-07-26 18:06:11 +0200

    12

--- template-checkout.php
+++ template-checkout.php
@@ -7,7 +7,6 @@ get_header(); ?>
 
 <main class="bp-main-content bp-checkout-page">
     <div class="bp-container">
-        <h1 class="bp-page-title">Finalizar compra</h1>
         <?php the_content(); ?>
     </div>
 </main>
